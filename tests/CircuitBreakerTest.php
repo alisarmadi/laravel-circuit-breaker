1ebae6f17875c5ec5143db7a631a4ab08198cc76
alisarmadi <ali.sarmadi.varzideh@gmail.com>
2024-01-02 23:42:00 +0330

    The 'orchestra/testbench' library added to the package.

--- tests/CircuitBreakerTest.php
+++ tests/CircuitBreakerTest.php
@@ -1,122 +1,29 @@
 <?php
 
+namespace SnappDoctor\CircuitBreaker\Tests;
+
 use SnappDoctor\CircuitBreaker\CircuitBreaker;
 use SnappDoctor\CircuitBreaker\Config;
 use Illuminate\Cache\Repository;
 
-it('should available & close', function () {
-    $config = getConfig();
-    $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
-    expect($circuitBreaker->isClose())->toBeTrue();
-    expect($circuitBreaker->isAvailable())->toBeTrue();
-});
-
-it('can increment errors', function () {
-    $config = getConfig();
-    $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    expect($circuitBreaker->getErrorsCount())->toBe(2);
-});
-
-it('should restart errors after mark success its in close state', function () {
-    $config = getConfig();
-    $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markSuccess();
-    expect($circuitBreaker->getErrorsCount())->toBe(0);
-});
-
-it('can be opened', function () {
-    $config = getConfig();
-    $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    expect($circuitBreaker->getErrorsCount())->toBe(5);
-    expect($circuitBreaker->isAvailable())->toBeFalse();
-    expect($circuitBreaker->isOpen())->toBeTrue();
-    expect($circuitBreaker->isClose())->toBeFalse();
-    expect($circuitBreaker->isHalfOpen())->toBeFalse();
-});
-
-it('can be half-opened', function () {
-    $config = getConfig();
-    $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $this->travel(31)->seconds();
-    expect($circuitBreaker->getErrorsCount())->toBe(0);
-    expect($circuitBreaker->isAvailable())->toBeTrue();
-    expect($circuitBreaker->isHalfOpen())->toBeTrue();
-    $this->travelBack();
-});
-
-it('can open again after the half-open mark failed ', function () {
-    $config = getConfig();
-    $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $this->travel(31)->seconds();
-    expect($circuitBreaker->getErrorsCount())->toBe(0);
-    expect($circuitBreaker->isAvailable())->toBeTrue();
-    expect($circuitBreaker->isHalfOpen())->toBeTrue();
-    $circuitBreaker->markFailed();
-    expect($circuitBreaker->isAvailable())->toBeFalse();
-    expect($circuitBreaker->isHalfOpen())->toBeFalse();
-    expect($circuitBreaker->isOpen())->toBeTrue();
-    $this->travelBack();
-});
-
-it('can close after half-open mark success', function () {
-    $config = getConfig();
-    $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $circuitBreaker->markFailed();
-    $this->travel(31)->seconds();
-    expect($circuitBreaker->getErrorsCount())->toBe(0);
-    expect($circuitBreaker->isAvailable())->toBeTrue();
-    expect($circuitBreaker->isHalfOpen())->toBeTrue();
-    $circuitBreaker->markSuccess();
-    expect($circuitBreaker->isAvailable())->toBeTrue();
-    expect($circuitBreaker->isHalfOpen())->toBeTrue();
-    expect($circuitBreaker->isOpen())->toBeFalse();
-    $circuitBreaker->markSuccess();
-    $circuitBreaker->markSuccess();
-    expect($circuitBreaker->isAvailable())->toBeTrue();
-    expect($circuitBreaker->isClose())->toBeTrue();
-    expect($circuitBreaker->isHalfOpen())->toBeFalse();
-    expect($circuitBreaker->isOpen())->toBeFalse();
-    $this->travelBack();
-});
-
-
-function getConfig()
+class CircuitBreakerTest extends TestCase
 {
-    return Config::fromArray(getDefaultConfig());
-}
+    /** @test */
+    public function test_circuit_breaker_should_available_and_close(): void
+    {
+        dd('sss');
+        $config = Config::fromArray([
+            'store' => config('cache.default'),
+            'time_window' => 6,
+            'error_threshold' => 5,
+            'error_timeout' => 30,
+            'half_open_timeout' => 15,
+            'success_threshold' => 3,
+        ]);
 
+        $circuitBreaker = new CircuitBreaker('test', $config, app(Repository::class));
 
-function getDefaultConfig(): array
-{
-    return [
-        'store' => config('cache.default'),
-        'time_window' => 6,
-        'error_threshold' => 5,
-        'error_timeout' => 30,
-        'half_open_timeout' => 15,
-        'success_threshold' => 3,
-    ];
+        $this->assertTrue($circuitBreaker->isClose());
+        $this->assertTrue($circuitBreaker->isAvailable());
+    }
 }
---
+++ tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SnappDoctor\CircuitBreaker\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use SnappDoctor\CircuitBreaker\CircuitBreakerServiceProvider;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            CircuitBreakerServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+//        config()->set('database.default', 'testing');
+    }
+}
